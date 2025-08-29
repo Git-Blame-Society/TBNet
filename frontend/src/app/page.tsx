845463494cc4f2feee1bcf9b578f39d02b002596
tanmoyyysarkar <tanmoysarkar.ts1234@gmail.com>
2025-08-30 01:20:15 +0530

    hello

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,10 @@
 import Link from 'next/link';
-import Navbar from './components/Navbar.tsx';
+import Navbar from './components/Navbar';
 const Home = () => {
   return (
     <>
       <Navbar />
-   
+      <h1>Hello</h1>
       <Link href="/dashboard">Go to Dashboard</Link>
     </>
   );
